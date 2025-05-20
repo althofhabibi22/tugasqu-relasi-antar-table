bdae0c9d77200d82492fecb85ce8a62accce6151
althofhabibi22 <althofhabibi26@gmail.com>
2025-05-20 12:37:34 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_tic";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if(!$koneksi) {
+    die ("koneksi gagal :" . mysqli_connect_error());
+}
+
+echo "koneksi berhasil";
+
+?>
